4b4a6f2033267ca910ca33eb2d9486008016768f
Sinku Kumar <sudosinku@gmail.com>
2023-07-06 21:47:31 +0530

    Movies data

--- app.py
+++ app.py
@@ -1,11 +1,39 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+movies_data = [
+    {
+        "name": "Ready Player One",
+        "cast": ["Tye Sheridan", "Olivia Cooke", "Ben Mendelsohn"],
+        "genre": "Sci-Fi"
+    },
+    {
+        "name": "The Matrix",
+        "cast": ["Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss"],
+        "genre": "Action"
+    },
+    {
+        "name": "The Lord of the Rings: The Fellowship of the Ring",
+        "cast": ["Elijah Wood", "Ian McKellen", "Liv Tyler"],
+        "genre": "Fantasy"
+    },
+    {
+        "name": "The Shawshank Redemption",
+        "cast": ["Tim Robbins", "Morgan Freeman", "Bob Gunton"],
+        "genre": "Drama"
+    },
+    {
+        "name": "The Godfather",
+        "cast": ["Marlon Brando", "Al Pacino", "James Caan"],
+        "genre": "Crime"
+    }
+]
 
-@app.route('/')
-def hello():
-    return {'hello': 'world'}
+
+@app.route('/movies')
+def get_movies():
+    return jsonify(movies_data)
 
 
 app.run()
